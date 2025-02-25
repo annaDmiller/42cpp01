c0987ec5c0c08e48fa77c9f23bcb592e87291b14
Anna Melnikova <amelniko@student.42.fr>
2025-02-25 21:43:34 +0100

    ex05 is done

---
+++ ex05/main.cpp
@@ -0,0 +1,29 @@
+#include "Harl.hpp"
+
+int main(void)
+{
+    Harl harl;
+
+    std::cout << "Check of HARLs' complains" << std::endl;
+    std::cout << std::endl;
+
+    harl.complain("DEBUG");
+    std::cout << std::endl;
+
+    harl.complain("INFO");
+    std::cout << std::endl;
+
+    harl.complain("WARNING");
+    std::cout << std::endl;
+
+    harl.complain("ERROR");
+    std::cout << std::endl;
+
+    harl.complain("debug");
+    std::cout << std::endl;
+
+    harl.complain("HELLO");
+    std::cout << std::endl;
+
+    return (0);
+}
